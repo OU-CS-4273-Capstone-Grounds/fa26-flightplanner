f102d537d3a45ac32407eb2589ab92352facc8b3
Sean Ropp <baconboy596@gmail.com>
2026-09-14 19:17:56 -0500

    CAPD-13 added 6 calculation functions and the unit tests
    Added pressure altitude, density altitude, take off/landing distances, true air speed  and rate of climb calculations and the unit tests for them.

---
+++ src/calculations.py
@@ -0,0 +1,95 @@
+def calculate_pressure_altitude(altimeter_setting, field_elevation):
+    """
+    Calculate the pressure altitude.
+
+    Parameters:
+    altimeter_setting (float): The current altimeter setting in inches of mercury (inHg).
+    field_elevation (float): The elevation of the airfield in feet.
+
+    Returns:
+    float: The pressure altitude in feet.
+    """
+
+    return field_elevation + (29.92 - altimeter_setting) * 1000
+
+def calculate_density_altitude(temperature, field_elevation, altimeter_setting):
+    """
+    Calculate the density altitude.
+
+    Parameters:
+    temperature (float): The outside air temperature in degrees Celsius.
+    field_elevation (float): The elevation of the airfield in feet.
+    altimeter_setting (float): The current altimeter setting in inches of mercury (inHg).
+    
+    Returns:
+    float: The density altitude in feet.
+    """ 
+
+    pressure_altitude = calculate_pressure_altitude(altimeter_setting, field_elevation)
+    ISA_temperature = 15 - (pressure_altitude / 1000) * 2
+    return pressure_altitude + (120 * (temperature - ISA_temperature))
+
+def calculate_rate_of_climb(temperature, field_elevation, altimeter_setting):
+    """
+    Calculate the rate of climb based on the density altitude from the piper cherokee chart.
+
+    Parameters:
+    temperature (float): The outside air temperature in degrees Celsius.
+    field_elevation (float): The elevation of the airfield in feet.
+    altimeter_setting (float): The current altimeter setting in inches of mercury (inHg).
+
+    Returns:
+    float: The rate of climb in feet per minute.
+    """
+
+    slope = 25
+    density_altitude = calculate_density_altitude(temperature, field_elevation, altimeter_setting)
+    return (16500-density_altitude)/slope
+
+def calculate_true_air_speed(temperature, field_elevation, altimeter_setting, power_setting):
+    """
+    Calculate the true air speed based on the density altitude and power setting.
+
+    Parameters:
+    temperature (float): The outside air temperature in degrees Celsius.
+    field_elevation (float): The elevation of the airfield in feet.
+    altimeter_setting (float): The current altimeter setting in inches of mercury (inHg).
+    power_setting (float): The power setting as a percentage.
+
+    Returns:
+    float: The calculated true air speed in miles per hour (mph).
+    """
+
+    density_altitude = calculate_density_altitude(temperature, field_elevation, altimeter_setting)
+    y_intercept = 45.653
+    mph_per_density_altitude = 0.0008722
+    mph_per_percent_power = 1.0430
+    return y_intercept + mph_per_density_altitude * density_altitude + mph_per_percent_power * power_setting
+
+def calculate_landing_distance(temperature, field_elevation, altimeter_setting, ground_roll, over_50_feet_obstacle):
+    """
+    Calculate the landing distance based on the density altitude and the type of landing.
+
+    Parameters:
+    temperature (float): The outside air temperature in degrees Celsius.
+    field_elevation (float): The elevation of the airfield in feet.
+    altimeter_setting (float): The current altimeter setting in inches of mercury (inHg).
+    ground_roll (bool): Whether to calculate the ground roll distance.
+    over_50_feet_obstacle (bool): Whether to calculate the distance to clear a 50-feet obstacle.
+
+    Returns:
+    float or str: The calculated landing distance in feet, or an error message if both or neither options are selected.
+    """
+
+    density_altitude = calculate_density_altitude(temperature, field_elevation, altimeter_setting)
+    if ground_roll and over_50_feet_obstacle:
+        return "Cannot have both enabled"
+    if ground_roll:
+        y_intercept = 37500
+        slope = 70
+        return (density_altitude/slope) + (y_intercept/slope)
+    if over_50_feet_obstacle:
+        y_intercept = 43000
+        slope = 40
+        return (density_altitude/slope) + (y_intercept/slope)
+    return "No option selected"
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,1 +1,111 @@
-# TODO: add tests
+import unittest
+ 
+from src.calculations import (
+    calculate_pressure_altitude,
+    calculate_density_altitude,
+    calculate_rate_of_climb,
+    calculate_takeoff_distance,
+    calculate_landing_distance,
+    calculate_true_air_speed,
+)
+
+class TestCalculations(unittest.TestCase):
+ 
+    def test_calculate_pressure_altitude(self):
+        altimeter_setting = 30.20
+        field_elevation = 1000
+        expected_pressure_altitude = 720
+        self.assertAlmostEqual(
+            calculate_pressure_altitude(altimeter_setting, field_elevation),
+            expected_pressure_altitude,
+            places=2,
+        )
+ 
+    def test_calculate_density_altitude(self):
+        temperature = 20
+        field_elevation = 10000
+        altimeter_setting = 30.20
+        expected_density_altitude = 3720
+        self.assertAlmostEqual(
+            calculate_density_altitude(temperature, field_elevation, altimeter_setting),
+            expected_density_altitude,
+            places=2,
+        )
+ 
+    def test_calculate_rate_of_climb(self):
+        temperature = 20
+        field_elevation = 10000
+        altimeter_setting = 30.20
+        expected_rate_of_climb = 260
+        self.assertAlmostEqual(
+            calculate_rate_of_climb(temperature, field_elevation, altimeter_setting),
+            expected_rate_of_climb,
+            places=2,
+        )
+ 
+    def test_calculate_takeoff_distance_ground_run(self):
+        expected = 11000 / 14  # 785.71
+        self.assertAlmostEqual(
+            calculate_takeoff_distance(15, 0, 29.92, True, False),
+            expected,
+            places=2,
+        )
+ 
+    def test_calculate_takeoff_distance_over_50_feet_obstacle(self):
+        expected = 10000 / 6  # 1666.67
+        self.assertAlmostEqual(
+            calculate_takeoff_distance(15, 0, 29.92, False, True),
+            expected,
+            places=2,
+        )
+ 
+    def test_calculate_takeoff_distance_both_enabled(self):
+        self.assertEqual(
+            calculate_takeoff_distance(15, 0, 29.92, True, True),
+            "Cannot have both enabled",
+        )
+ 
+    def test_calculate_takeoff_distance_neither_enabled(self):
+        self.assertEqual(
+            calculate_takeoff_distance(15, 0, 29.92, False, False),
+            "No option selected",
+        )
+ 
+    def test_calculate_landing_distance_ground_roll(self):
+        expected = 37500 / 70  # 535.71
+        self.assertAlmostEqual(
+            calculate_landing_distance(15, 0, 29.92, True, False),
+            expected,
+            places=2,
+        )
+ 
+    def test_calculate_landing_distance_over_50_feet_obstacle(self):
+        expected = 43000 / 40  # 1075.0
+        self.assertAlmostEqual(
+            calculate_landing_distance(15, 0, 29.92, False, True),
+            expected,
+            places=2,
+        )
+ 
+    def test_calculate_landing_distance_both_enabled(self):
+        self.assertEqual(
+            calculate_landing_distance(15, 0, 29.92, True, True),
+            "Cannot have both enabled",
+        )
+ 
+    def test_calculate_landing_distance_neither_enabled(self):
+        self.assertEqual(
+            calculate_landing_distance(15, 0, 29.92, False, False),
+            "No option selected",
+        )
+ 
+    def test_calculate_true_air_speed(self):
+        expected = 45.653 + 0.0008722 * 0 + 1.0430 * 75  # 123.878
+        self.assertAlmostEqual(
+            calculate_true_air_speed(15, 0, 29.92, 75),
+            expected,
+            places=2,
+        )
+ 
+if __name__ == "__main__":
+    unittest.main()
